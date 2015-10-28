fa826c8ac79cce76af90bce73602338e43bfabb5
Can Ceran <can@ceran.email>
2015-10-28 20:11:56 +0200

    Initial Commit

---
+++ iSquare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iSquare
+//
+//  Created by Can Ceran on 28/10/15.
+//  Copyright © 2015 Ceran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
